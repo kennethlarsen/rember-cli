da8cb16f2d7a838bcd4d8d51c1114664a7eace3a
Kenneth Larsen <kla@linkfire.com>
2019-04-26 12:16:08 +0200

    Add support for package name

---
+++ src/utils.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, Read, Write};
+use std::path::Path;
+
+pub fn update_values_in_files(word_to_replace: &str, new_word: &str, path: &str) -> Result<(), io::Error> {
+    let mut src = File::open(path)?;
+    let mut data = String::new();
+    src.read_to_string(&mut data)?;
+    drop(src);  // Close the file early
+
+    let new_data = data.replace(word_to_replace, new_word);
+    let mut dst = File::create(path)?;
+    dst.write(new_data.as_bytes())?;
+
+    Ok(())
+}
